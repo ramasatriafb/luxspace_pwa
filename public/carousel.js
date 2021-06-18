3f5454732d533d6326c3aef35fb6a9338c9eb63b
Rama Firmansyah <ramasatriafb@gmail.com>
2021-06-18 12:00:18 +0700

    Update Carousel and minor bug

--- public/carousel.js
+++ public/carousel.js
@@ -1,8 +1,10 @@
-const carouselId = document.getElementById("carousel");
+import { addClass, removeClass } from "./utils-class";
+
+const carouselId = document?.getElementById("carousel");
 const carouselItems = carouselId?.getElementsByClassName("flex")[0];
 const carouselContainer = carouselId?.getElementsByClassName("container")[0];
 
-function carousel_calc_offset(e) {
+function carouselCalculateOffset() {
   const carouselOffset = carouselContainer.getBoundingClientRect().left;
 
   carouselItems.style.paddingLeft = `${carouselOffset - 16}px`;
@@ -15,7 +17,7 @@ function slide(wrapper, items) {
     posInitial,
     posFinal,
     threshold = 100,
-    itemToShow = window.innerWidth < 767 ? 1 : 4,
+    itemToShow = 4,
     slides = items.getElementsByClassName("card"),
     slidesLength = slides.length,
     slideSize = items.getElementsByClassName("card")[0].offsetWidth,
@@ -24,18 +26,12 @@ function slide(wrapper, items) {
 
   wrapper.classList.add("loaded");
 
-  // Mouse events
   items.onmousedown = dragStart;
 
-  // Touch events
   items.addEventListener("touchstart", dragStart);
   items.addEventListener("touchend", dragEnd);
   items.addEventListener("touchmove", dragAction);
-  items.addEventListener("click", function (event) {
-    !allowShift && event.preventDefault();
-  });
 
-  // Transition events
   items.addEventListener("transitionend", checkIndex);
 
   function dragStart(e) {
@@ -44,6 +40,7 @@ function slide(wrapper, items) {
     posInitial = items.offsetLeft;
 
     if (e.type == "touchstart") {
+      console.log(e.touches);
       posX1 = e.touches[0].clientX;
     } else {
       posX1 = e.clientX;
@@ -62,11 +59,13 @@ function slide(wrapper, items) {
       posX2 = posX1 - e.clientX;
       posX1 = e.clientX;
     }
-    items.style.left = items.offsetLeft - posX2 + "px";
+
+    items.style.left = `${items.offsetLeft - posX2}px`;
   }
 
-  function dragEnd(e) {
+  function dragEnd() {
     posFinal = items.offsetLeft;
+
     if (posFinal - posInitial < -threshold) {
       shiftSlide(1, "drag");
     } else if (posFinal - posInitial > threshold) {
@@ -79,56 +78,50 @@ function slide(wrapper, items) {
     document.onmousemove = null;
   }
 
-  function shiftSlide(dir, action) {
-    items.classList.add("transition-all");
-    items.classList.add("duration-200");
+  function shiftSlide(direction, action) {
+    addClass(items, "transition-all duration-200");
 
     if (allowShift) {
-      if (!action) {
-        posInitial = items.offsetLeft;
-      }
+      if (!action) posInitial = items.offsetLeft;
 
-      if (dir == 1) {
-        items.style.left = posInitial - slideSize + "px";
+      if (direction == 1) {
+        items.style.left = `${posInitial - slideSize}px`;
         index++;
-      } else if (dir == -1) {
-        items.style.left = posInitial + slideSize + "px";
+      } else if (direction == -1) {
+        items.style.left = `${posInitial + slideSize}px`;
         index--;
       }
     }
-    // console.log(index);
 
     allowShift = false;
   }
 
   function checkIndex() {
     setTimeout(() => {
-      items.classList.remove("transition-all");
-      items.classList.remove("duration-200");
+      removeClass(items, "transition-all duration-200");
     }, 200);
-    // console.log("index:", index);
-    const isMobile = window.innerWidth < 767 ? 0 : -1;
-    if (index < 0) {
-      // console.log(1);
+
+    if (index == -1) {
+      items.style.left = -(slidesLength * slideSize) + "px";
+      index = slidesLength - 1;
+    }
+
+    if (index == slidesLength - itemToShow) {
+      items.style.left = -((slidesLength - itemToShow - 1) * slideSize) + "px";
+      index = slidesLength - itemToShow - 1;
+    }
+
+    if (index == slidesLength || index == slidesLength - 1) {
       items.style.left = "0px";
       index = 0;
-    } else if (index >= slidesLength - itemToShow) {
-      // console.log(2);
-      items.style.left =
-        -((slidesLength - itemToShow + isMobile) * slideSize) + "px";
-      index = slidesLength - itemToShow;
-    } else if (index == slidesLength || index == slidesLength - 1) {
-      // console.log(3);
-      items.style.left = slidesLength - 1 * slideSize + "px";
-      index = slidesLength - 1;
     }
 
     allowShift = true;
   }
 }
 
-if (document.getElementById("carousel")) {
+if (carouselId) {
   slide(carouselId, carouselItems);
-  window.addEventListener("load", carousel_calc_offset);
-  window.addEventListener("resize", carousel_calc_offset);
+  window.addEventListener("load", carouselCalculateOffset);
+  window.addEventListener("resize", carouselCalculateOffset);
 }
